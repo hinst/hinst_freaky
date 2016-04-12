23e9b6b698cf53b4189bf1a617f7bda6d617e573
hinst <hinstance@yandex.ru>
2016-04-12 15:24:18 +0300

    meow

--- src/program.rs
+++ src/program.rs
@@ -1,9 +1,14 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
   println!("GLOBAL START");
   let mut text = String::new();
   io::stdin().read_line(&mut text).expect("Failed to read line");
   println!("Received text: '{}'", text);
+  let random_number = rand::thread_rng().gen_range(0, 10);
+  println!("random_number = {}", random_number);
   println!("GLOBAL EXIT")
 }
