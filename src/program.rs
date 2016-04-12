b23c40c82795a94ae75dd41eadfd0a7a65cfde26
hinst <hinstance@yandex.ru>
2016-04-12 14:51:11 +0300

    test

--- src/program.rs
+++ src/program.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
   println!("GLOBAL START...");
+  let mut text = String::new();
+  io::stdin().read_line(&mut text).expect("Failed to read line");
+  println!("Received text: '{}'", text);
+  println!("GLOBAL EXIT")
 }
