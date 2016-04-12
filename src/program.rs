72a7fee3683e6ea84adb46a5063a2be5310bda75
hinst <hinstance@yandex.ru>
2016-04-12 15:06:13 +0300

    + rand

--- src/program.rs
+++ src/program.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 fn main() {
-  println!("GLOBAL START...");
+  println!("GLOBAL START");
   let mut text = String::new();
   io::stdin().read_line(&mut text).expect("Failed to read line");
   println!("Received text: '{}'", text);
